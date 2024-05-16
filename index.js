730a08a66479b42dacb9dea9b8ef786853f0ece1
Zach Latta <zach@zachlatta.com>
2024-05-15 20:58:48 -0400

    Get Hack Clubbers updating

--- index.js
+++ index.js
@@ -50,16 +50,130 @@ let airtableFieldMappingRules = await new Promise((resolve, reject) => {
   )
 })
 
-let programMappingRules = airtableProgramMappingRules.map(rawRule => {
-  let rule = {}
-  rule[rawRule['Loops.so Field To Map']] = rawRule['Program'][0]
-  return rule
+let programMappingRules = {}
+airtableProgramMappingRules.forEach(rawRule => {
+  programMappingRules[rawRule['Loops.so Field To Map']] = rawRule['Program'][0]
 })
 
-let fieldMappingRules = airtableFieldMappingRules.map(rawRule => {
-  let rule = {}
-  rule[rawRule['Loops.so Field To Map']] = rawRule['Hack Clubber Field']
-  return rule
+let fieldMappingRules = {}
+airtableFieldMappingRules.forEach(rawRule => {
+  fieldMappingRules[rawRule['Loops.so Field To Map']] = rawRule['Hack Clubber Field']
 })
 
-console.log(fieldMappingRules)
+console.log(`Processing Loops export:`)
+
+let test = loopsData.slice(0, 250)
+
+let updateQueue = []
+let createQueue = []
+
+for (let row of test) {
+  console.log(`  ${row.email}`)
+
+  if (row.userGroup != 'Hack Clubber') {
+    console.log("    Skipping because not Hack Clubber")
+    continue
+  }
+
+  let airtableMatch = await new Promise((resolve, reject) => {
+    base('Hack Clubbers').select({
+      filterByFormula: `{${fieldMappingRules.email}} = '${row.email}'`
+    }).firstPage((err, records) => {
+      if (err) return reject(err)
+
+      if (records.length == 0) {
+        resolve(null)
+      } else {
+        let match = records[0].fields
+        match.createdTime = new Date(records[0]._rawJson.createdTime)
+        match.id = records[0].id
+
+        resolve(match)
+      }
+    })
+  })
+
+  let airtableUpdates = {}
+
+  for (let loopsField in fieldMappingRules) {
+    let airtableField = fieldMappingRules[loopsField]
+
+    if (row[loopsField]) {
+      airtableUpdates[airtableField] = row[loopsField]
+    }
+  }
+
+  airtableUpdates['Programs'] = []
+
+  for (let loopsField in programMappingRules) {
+    let airtableProgramId = programMappingRules[loopsField]
+
+    if (row[loopsField] && row[loopsField] != "") {
+      airtableUpdates['Programs'].push(airtableProgramId)
+    }
+  }
+
+  // convert dates back to strings before sending to airtable
+  for (let key in airtableUpdates) {
+    let val = airtableUpdates[key]
+
+    if (val instanceof Date) {
+      airtableUpdates[key] = val.toISOString()
+    }
+  }
+
+  if (airtableMatch) {
+    updateQueue.push({
+      id: airtableMatch.id,
+      fields: airtableUpdates
+    })
+  } else {
+    createQueue.push({
+      fields: airtableUpdates
+    })
+  }
+
+  if (updateQueue.length == 10) {
+    await new Promise((resolve, reject) => {
+      base('Hack Clubbers').update(updateQueue, (err, _) => {
+        if (err) reject(err)
+        resolve()
+      })
+    })
+
+    updateQueue = []
+  }
+
+  if (createQueue.length == 10) {
+    await new Promise((resolve, reject) => {
+      base('Hack Clubbers').create(createQueue, (err, _) => {
+        if (err) reject(err)
+        resolve()
+      })
+    })
+
+    createQueue = []
+  }
+}
+
+if (updateQueue.length > 0) {
+  await new Promise((resolve, reject) => {
+    base('Hack Clubbers').update(updateQueue, (err, _) => {
+      if (err) reject(err)
+      resolve()
+    })
+  })
+
+  updateQueue = []
+}
+
+if (createQueue.length > 0) {
+  await new Promise((resolve, reject) => {
+    base('Hack Clubbers').create(createQueue, (err, _) => {
+      if (err) reject(err)
+      resolve()
+    })
+  })
+
+  createQueue = []
+}
